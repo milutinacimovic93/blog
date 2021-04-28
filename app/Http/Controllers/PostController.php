0c67527c4530017a65edf1c73be38e1ad39e6b3b
milutinacimovic93 <milutinacimovic93@gmail.com>
2021-04-28 15:38:15 +0200

    Kreiranje Post modela, Post kontrolera i migracija, dve view stranice jedna izlistava sve postove durga klikom na post vodi na single post

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+  public function index()
+  {
+    $posts = Post::all(); 
+    return view('posts', compact('posts'));
+  }
+
+  public function show($id)
+  {
+    $post = Post::findOrFail($id);
+    return view('post', compact('post'));
+  }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\PostController;
+use App\Models\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/{id}', [PostController::class, 'show']);
